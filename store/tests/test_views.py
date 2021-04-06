7caf34ec69aad26d0cf2365725c0a044bd11ec6f
alexfiel07 <alexfiel07@gmail.com>
2021-04-06 20:37:22 +0800

    innitial commit

---
+++ store/tests/test_views.py
@@ -0,0 +1,19 @@
+from unittest import skip
+
+from django.test import TestCase
+from django.contrib.auth.models import User
+from store.models import Category, Product
+from django.test.client import Client
+
+from store.models import Category, Product 
+
+class TestViewResponses(TestCase):
+    def setUp(self):
+        self.c = Client()
+    
+    def test_url_allowed_host(self):
+        """
+        TEST ALLOWED HOSTS
+        """
+        response = self.c.get('/')
+        self.assertEqual(response.status_code, 200)
